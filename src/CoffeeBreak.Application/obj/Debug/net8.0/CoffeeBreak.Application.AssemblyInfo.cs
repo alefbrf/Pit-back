a9fe8096de9ea72102249b6a5b28a5736bb2f256
Alef Frade <alef.frade.01tec@gmail.com>
2024-11-30 18:01:23 -0300

    second

--- src/CoffeeBreak.Application/obj/Debug/net8.0/CoffeeBreak.Application.AssemblyInfo.cs
+++ src/CoffeeBreak.Application/obj/Debug/net8.0/CoffeeBreak.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeBreak.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eef296e548803018e72125384906be85fa775ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531c4af7086ab4e35ff1aba1b74559725aa52cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeBreak.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeBreak.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
